90b0699dbb196c7d10a9051a9140d14e79f0ba77
hilmiii <hilmiawananggoro189@gmail.com>
2025-12-14 19:37:17 +0700

    bismillah gachor 3

--- src/Components/layout/Header.jsx
+++ src/Components/layout/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/Components/ui/button';
 import { ShoppingCart, Menu, X, User, Beaker, LogOut } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
